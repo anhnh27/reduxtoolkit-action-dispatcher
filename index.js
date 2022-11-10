85a3af67644afcdea43baf4b9afdeabea0e32c75
Alex Nguyen <nguyenhoanganh10290@gmail.com>
2022-11-11 00:47:58 +1100

    - update npm cicd

--- index.js
+++
@@ -1,6 +0,0 @@
-import { actionDispatcherMiddleware } from "./lib";
-import createDispatcher from "./src/createDispatcher";
-
-export { createDispatcher };
-
-export default actionDispatcherMiddleware;
